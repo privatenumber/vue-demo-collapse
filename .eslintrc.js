96e05608c33a908021b7d7b0b1444621ed63685e
Hiroki Osame <osame@squareup.com>
2020-03-30 18:28:53 -0400

    Added linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,18 @@ module.exports = {
 	root: true,
 
 	env: {
-		node: true
+		browser: true,
 	},
 
 	extends: [
 		'airbnb-base',
+		'plugin:vue/recommended',
 	],
 
 	rules: {
 		indent: 'off',
 		'indent-legacy': ['error', 'tab'],
 		'no-tabs': 'off',
+		'vue/html-indent': ['error', 'tab'],
 	},
 };
